69bf7da2ef4260c15791b9b4fb15803010ef5c46
ashwini82 <ashwiniatharv1982@gmail.com>
2020-04-27 21:54:28 -0400

    third commit

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -52,6 +52,17 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
+        const locations = [
+            [1, 1],
+            [2, 1],
+            [3, 1],
+            [1, 2],
+            [2, 2],
+            [3, 2],
+            [1, 3],
+            [2, 3],
+            [3, 3]
+        ];
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
@@ -63,7 +74,7 @@ class Game extends React.Component {
             history: history.concat([
                 {
                     squares: squares,
-                    latestMoveSquare: i
+                    location: locations[i],
                 }
             ]),
 
@@ -87,14 +98,16 @@ class Game extends React.Component {
             const col = 1 + latestMoveSquare % 3;
             const row = 1 + Math.floor(latestMoveSquare / 3);
             const desc = move ?
-                'Go to move #' + move :
+                'Go to move #' + move + " @ " + history[move].location :
                 'Go to game start';
             return (
                 <li key={move}>
-                    <button
-                        className={move === stepNumber ? 'move-list-item-selected' : ''}
-                        onClick={() => this.jumpTo(move)}>{desc}
+
+                    <button onClick={() => this.jumpTo(move)}>
+                        {move == this.state.stepNumber ? <b>{desc}</b> : desc}
                     </button>
+
+
                 </li>
             );
         });
